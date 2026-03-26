94224f4e3e89343ffa800a349de1362e784ff8ec
Sofia <irenesofia.lotti@studio.unibo.it>
2026-03-26 16:39:23 +0100

    implemented task 1

--- src/main/scala/it/unibo/pps/tasks/adts/Ex1ComplexNumber.scala
+++ src/main/scala/it/unibo/pps/tasks/adts/Ex1ComplexNumber.scala
@@ -19,12 +19,22 @@ object Ex1ComplexNumbers:
 
   object BasicComplexADT extends ComplexADT:
 
-    // Change assignment below: should probably define a case class and use it?
-    type Complex = Nothing 
-    def complex(re: Double, im: Double): Complex = ???
+    private case class ComplexImpl(re: Double, im: Double)
+
+    opaque type Complex = ComplexImpl
+    def complex(re: Double, im: Double): Complex = ComplexImpl(re, im)
     extension (complex: Complex)
-      def re(): Double = ???
-      def im(): Double = ???
-      def sum(other: Complex): Complex = ???
-      def subtract(other: Complex): Complex = ???
-      def asString(): String = ???
+      def re(): Double = complex match
+        case ComplexImpl(re, _) => re
+      def im(): Double = complex match
+        case ComplexImpl(_, im) => im
+      def sum(other: Complex): Complex = (complex, other) match
+        case (ComplexImpl(re1, im1), ComplexImpl(re2, im2)) => ComplexImpl(re1 + re2, im1 + im2)
+      def subtract(other: Complex): Complex = (complex, other) match
+        case (ComplexImpl(re1, im1), ComplexImpl(re2, im2)) => ComplexImpl(re1 - re2, im1 - im2)
+      def asString(): String = complex match
+        case ComplexImpl(re, 0) => s"$re"
+        case ComplexImpl(0, im) => s"${im}i"
+        case ComplexImpl(re, im) =>
+          val op = if im < 0 then "-" else "+"
+          s"$re $op ${math.abs(im)}i"
